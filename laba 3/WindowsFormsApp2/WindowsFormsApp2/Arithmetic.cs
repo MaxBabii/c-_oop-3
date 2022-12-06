1dc243e8120275bd6ce237de6dfc0ab56cde3179
Maksym <90716258+MaxBabii@users.noreply.github.com>
2022-12-06 02:15:42 -0800

    Add files via upload

---
+++ laba 3/WindowsFormsApp2/WindowsFormsApp2/Arithmetic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_class
+{
+    internal abstract class Class_base
+    {
+        protected int[] arr;
+        private int num;
+        private int step;
+
+        protected Class_base(int value)
+        {
+            arr = new int [value];
+        }
+        public int Num
+            { get { return num; } set { num = value; } }
+
+        public virtual int Step
+            { get { return step; } set { step = value; } }
+
+        public int[] return_elements()
+        {
+            return arr;
+        }
+        public abstract int sum(int k);
+        public abstract int k_elem(int k);
+    }
+    class Class_arifm : Class_base
+    {
+        public Class_arifm(int x): base(x) { }
+
+        public override int Step { get => base.Step; set => base.Step = value; }
+
+        public override int sum(int k)
+        {
+            return 0;
+        }
+        public override int k_elem(int k)
+        {
+            return 0;
+        }
+        private int next(int prev, int step)
+        {
+            return prev + step;
+        }
+        public void array_value()
+        {
+            arr[0] = Num;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = next(arr[i - 1], Step);
+            }
+        }
+    }
+}
---
+++ laba 3/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using New_class;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Class_arifm res = new Class_arifm(0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = ((int)numericUpDown3.Value);
+            int first_elem = ((int)numericUpDown1.Value);
+            int step = ((int)numericUpDown2.Value);
+
+            res = new Class_arifm(count);
+            res.Step = step;
+            res.Num = first_elem;
+            res.array_value();
+            textBox1.Text = "";
+
+            for(int i = 0; i < res.return_elements().Length; i++)
+            {
+                textBox1.Text += res.return_elements()[i] + " ";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] result = res.return_elements();
+            MessageBox.Show($"Значення {((int)numericUpDown4.Value)} - го елемента послідовності: {result[((int)numericUpDown4.Value)-1]}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] result_sum = res.return_elements();
+            int sum = 0;
+            for(int i=0; i < result_sum.Length; i++)
+            {
+                sum += result_sum[i];
+            }
+            MessageBox.Show($"Сума арифметичної послідовності: {sum}");
+        }
+    }
+}
